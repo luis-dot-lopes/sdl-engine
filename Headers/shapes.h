668d10eab0ff5d8e51fd88af19f8dfff37977626
luis-dot-lopes <luisantonio20054@gmail.com>
2021-05-28 15:00:38 -0300

    Added function for getting the intersection between two rects

--- Headers/shapes.h
+++ Headers/shapes.h
@@ -1,6 +1,8 @@
 #ifndef SHAPES_H_INCLUDED
 #define SHAPES_H_INCLUDED
 
+#include <math.h>
+
 typedef struct {
     double x, y, width, height;
 } GE_Rect;
@@ -10,5 +12,11 @@ bool GE_Rect_collided(GE_Rect rect1, GE_Rect rect2);
 
 //Gets a rect representing intersection between two other rects
 GE_Rect GE_Rect_intersection(GE_Rect rect1, GE_Rect rect2);
+
+//Draws a rect
+void GE_Rect_draw(GE_Rect rect);
+
+//Fills a rect
+void GE_Rect_fill(GE_Rect rect);
 
 #endif // SHAPES_H_INCLUDED
--- Sources/shapes/rect.c
+++ Sources/shapes/rect.c
@@ -9,4 +9,14 @@ bool GE_Rect_collided(GE_Rect rect1, GE_Rect rect2) {
 
 GE_Rect GE_Rect_intersection(GE_Rect rect1, GE_Rect rect2) {
 
+    if(GE_Rect_collided(rec1, rect2)) {
+        double maxY = rect1.y > rect2.y ? y : rect2.y;
+        double minXcorner = rect1.x > rect2.x ? rect2.x + rect2.width : rect1.x + rect1.width;
+        double maxX = rect1.x > rect2.x ? x : rect2.x;
+        double minYcorner = rect1.y > rect2.y ? rect2.y + rect2.height : rect1.y + rect1.height;
+        return { maxX, maxY, abs(minXcorner - maxX), abs(minYcorner - maxY) };
+    } else {
+        return { 0, 0, 0, 0 };
+    }
+
 }
